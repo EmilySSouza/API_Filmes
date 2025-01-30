a4dae8a696391ccacb17a03d15485f4c8447ee0e
Emily Souza <emilyssouza05@gmail.com>
2025-01-30 19:48:01 -0300

    feat: User table adjustments

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -70,21 +70,6 @@ class UsersController {
 
     return response.json();
     }
-
-    async delete(request, response) {
-        const { id } = request.params;
-    
-        const database = await sqliteConnection();
-        const user = await database.get("SELECT * FROM users WHERE id = ?", [id]);
-    
-        if (!user) {
-            throw new AppError("Usuário não encontrado");
-        }
-    
-        await database.run("DELETE FROM users WHERE id = ?", [id]);
-    
-        return response.json();
-    }
 }
 
 module.exports = UsersController;
